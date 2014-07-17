f2607fa2d65c38d76ccd5442b9582d19528da1b0
titovanton <mail@titovanton.com>
2014-07-18 05:54:08 +0600

    clean django-node

--- README.md
+++ README.md
@@ -8,4 +8,15 @@ Ubuntu 14.04 LTS:
 
 ## Create Django-node
 
-    
+Create a brand new django-node image if you did not yet:
+
+    sudo docker build -t $USER/django-node:brand-new https://raw.githubusercontent.com/titovanton/bicycle-docker/master/django-node/Dockerfile
+
+Launch container:
+
+    sudo docker run -i -t $USER/django-node:brand-new /bin/bash
+
+Create Django project, do not forget rename `PROJECT_NAME` variable:
+
+    export PROJECT_NAME=myproject
+    curl -s  https://raw.githubusercontent.com/titovanton/bicycle-docker/master/django-node/create_project.sh | sudo bash
--- django-node/create_project.sh
+++ django-node/create_project.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
-
 if [[ $# -ne 1 ]]; then
     echo "Usage:"
     echo "export PROJECT_NAME=<project_name>"
-    https://raw.githubusercontent.com/titovanton/bicycle-docker/master/django-node/create_project.sh
-    echo '$ curl -s  /path/to/create_project.sh'
+    echo "curl -s https://raw.githubusercontent.com/titovanton/bicycle-docker/master/django-node/create_project.sh | sudo sh"
     exit 1
-else
-    PROJECT_NAME=$1
 fi
 
-# setting up uwsgi.ini from template (imperor mode)
+# uwsgi
 if [ ! -f /etc/uwsgi/uwsgi_params ]; then
-    cp /webapps/bicycle-docker/uwsgi_params /etc/uwsgi/uwsgi_params
+    curl -s https://raw.githubusercontent.com/nginx/nginx/master/conf/uwsgi_params > /etc/uwsgi/uwsgi_params
 fi
 echo 'touch this file to reload vassal' > $PROJECT_DIR/reload_uwsgi
 sed -e "s;%PROJECT_NAME%;$PROJECT_NAME;g" \
