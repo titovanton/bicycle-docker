bb1bfb35f74846dae6e0b5e207082048c0cf260b
titovanton <mail@titovanton.com>
2014-07-18 18:56:57 +0600

    setup sabma

--- samba-node/setup.sh
+++ samba-node/setup.sh
@@ -6,8 +6,8 @@ if [[ $USER_NAME == '' ]]; then
     echo 'User name can not be blank'
     exit 1
 fi
-docker -rm build -t $repo/samba-node:brand-new https://raw.githubusercontent.com/titovanton/bicycle-docker/master/samba-node/Dockerfile
-last=$(sudo docker ps -l -q)
-docker run -t -i $last "useradd --no-log-init --no-create-home --no-user-group $USER_NAME && smbpasswd -a $USER_NAME && smbpasswd -e $USER_NAME"
-last=$(sudo docker ps -l -q)
-docker commit $last $repo/samba-node:$USER_NAME
+id=$(docker --rm build -t $repo/samba-node:brand-new https://raw.githubusercontent.com/titovanton/bicycle-docker/master/samba-node/Dockerfile)
+docker run -t -i $id "useradd --no-log-init --no-create-home --no-user-group $USER_NAME && smbpasswd -a $USER_NAME && smbpasswd -e $USER_NAME"
+id=$(sudo docker ps -l -q)
+docker commit $id $repo/samba-node:$USER_NAME
+docker rm $id
