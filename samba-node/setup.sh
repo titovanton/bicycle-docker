28da71232c7600d3b26f8d3aff0a3e14ab925d2b
titovanton <mail@titovanton.com>
2014-07-18 13:54:16 +0600

    setup sabma

---
+++ samba-node/setup.sh
@@ -0,0 +1,10 @@
+read -p "Enter name of samba user (required): " USER_NAME
+if [[ $USER_NAME == '' ]]; then
+    echo 'User name can not be blank'
+    exit 1
+fi
+docker build -t titovanton/samba-node:brand-new https://raw.githubusercontent.com/titovanton/bicycle-docker/master/samba-node/Dockerfile
+last=$(sudo docker ps -l -q)
+docker run -t -i $last "useradd --no-log-init --no-create-home --no-user-group $USER_NAME; smbpasswd -a $USER_NAME; smbpasswd -e $USER_NAME"
+last=$(sudo docker ps -l -q)
+docker commit $last titovanton/samba-node:$USER_NAME
