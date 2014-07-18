eca76e043ae9a11cec9c8a8b5349ae27cb446e7c
titovanton <mail@titovanton.com>
2014-07-18 20:20:24 +0600

    setup sabma test final beta

--- samba-node/create_user.sh
+++ samba-node/create_user.sh
@@ -6,5 +6,5 @@ else
     USERNAME=$1
 fi
 useradd --no-log-init --no-create-home --no-user-group $USERNAME
-# smbpasswd -a $USERNAME
-# smbpasswd -e $USERNAME
+smbpasswd -a $USERNAME
+smbpasswd -e $USERNAME
